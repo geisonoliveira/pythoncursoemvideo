35f0b92d2955d40a53e981cb9c1a3874a2967551
geisonoliveira <gs_gaara@hotmail.com>
2021-01-24 22:16:46 -0300

    teste

--- 6.py
+++ 6.py
@@ -2,4 +2,4 @@ n = int(input('Digite um numero '))
 d = n * 2
 t = n * 3
 r = n**(1/2)
-print('O dobro de {} é {} e seu triplo é {} e sua raiz quadrada é {}'.format(n, d, t, r))
+print('O dobro de {} é {} e seu triplo é {} e sua raiz quadrada é {:.2f}'.format(n, d, t, r))
